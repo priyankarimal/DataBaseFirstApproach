d38b9dddc4a46893c1f62c00171b6fb783907f67
Priyanka <priyankarimal87@gmail.com>
2024-09-12 15:51:15 +0545

    Add project files.

---
+++ DataBaseFirstApproach/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using DataBaseFirstApproach.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DataBaseFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CodeFirstDbContext context;
+
+        public HomeController(ILogger<HomeController> logger, CodeFirstDbContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
